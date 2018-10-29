83b802c82476f87b11b3af89b786c3561ad176b1
Danielle Ali <dani.maria.ali@gmail.com>
2018-10-29 14:50:50 -0400

    Blog Clone Project
    Django Practice, created simple blog site.

---
+++ mysite/blog/models.py
@@ -0,0 +1,44 @@
+from django.db import models
+from django.utils import timezone
+from django.urls import reverse
+
+# Create your models here.
+
+class Post(models.Model):
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    title = models.CharField(max_length = 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default = timezone.now())
+    published_date = models.DateTimeField(blank=True, null = True)
+
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def approve_comment(self):
+        return self.comments.filter(approved_comment = True)
+
+    #This defines where they will go once they create a post
+    def get_absolute_url(self):
+        return reverse('post_detail', kwargs={'pk':self.pk})
+
+    def __str__ (self):
+        return self.title
+
+class Comment(models.Model):
+    post = models.ForeignKey('blog.Post', related_name = 'comments', on_delete = models.CASCADE)
+    author = models.CharField(max_length= 200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now())
+    approved_comment = models.BooleanField(default = False)
+
+    def approve(self):
+        self.approved_comment = True
+        self.save()
+
+    #define where they will be redirected once they create a comment
+    def get_absolute_url(self):
+        return reverse('post_list')
+
+    def __str__(self):
+        return self.text
